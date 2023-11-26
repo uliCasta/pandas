687d3f3964c58484d72c4db8068df8b16174634b
Juan Sebastian Castañeda Burbano <sicastaprojectos@gmail.com>
2023-11-26 13:16:50 -0500

    xd

--- main.py
+++ main.py
@@ -1,69 +1,67 @@
-import glob
 import pandas as pd
-import numpy
-from pandas import DataFrame
-from os.path import basename
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 LOCATION_ROW = "location"
-
-
-def csv_data_frames(dir_path: str) -> list[DataFrame]:
-    files_paths = glob.glob(dir_path)
-    files_list = []
-
-    for file_path in files_paths:
-        data = pd.read_csv(file_path)
-        filename_ext = basename(file_path)
-        location = filename_ext.split(".")[0].split("_")[-1]
-        data[LOCATION_ROW] = location
-
-        files_list.append(data)
-
-    return files_list
-
-
+AVERAGE_CLIENT_PER_DAY = "average_client_day"
+AVERAGE_EARNINGS_PER_DAY = "average_earnings_day"
 MY_COLUMNS = {
     "NOMBRE": "name",
     "TELEFONO": "phone",
     "EMAIL": "email",
     "DIRECCION": "direction",
     "TIPO DE ESTABLECIMIENTO": "type_establishment",
+    "LOACION": LOCATION_ROW,
+    "PROMEDIO DE GANANCIAS POR DIA": AVERAGE_EARNINGS_PER_DAY,
+    "PROMEDIA DE CLIENTES POR DIA": AVERAGE_CLIENT_PER_DAY,
 }
 MY_COLUMNS_TYPES = {
-    MY_COLUMNS["NOMBRE"]: "string",
-    MY_COLUMNS["TELEFONO"]: "string",
-    MY_COLUMNS["EMAIL"]: "string",
-    MY_COLUMNS["DIRECCION"]: "string",
-    MY_COLUMNS["TIPO DE ESTABLECIMIENTO"]: "string",
+    "name": "string",
+    "phone": "string",
+    "email": "string",
+    "direction": "string",
+    "type_establishment": "string",
     LOCATION_ROW: "string",
+    AVERAGE_CLIENT_PER_DAY: "int",
+    AVERAGE_EARNINGS_PER_DAY: "int",
 }
 
-rs_dfs = csv_data_frames("./data/*.csv")
-r_df = pd.concat(rs_dfs, ignore_index=True)
+r_df = pd.read_csv("./data/industria_gastronomica.csv")
 r_df = r_df.rename(columns=MY_COLUMNS)
-r_df = r_df.replace(numpy.nan, "N/A")
 r_df = r_df.astype(MY_COLUMNS_TYPES)
+r_df = r_df.replace(np.nan, "n/a")
 
-
-r_df[MY_COLUMNS["NOMBRE"]] = r_df[MY_COLUMNS["NOMBRE"]].str.lower()
-
-r_df[MY_COLUMNS["DIRECCION"]] = r_df[MY_COLUMNS["DIRECCION"]].str.lower()
-
-r_df[MY_COLUMNS["TIPO DE ESTABLECIMIENTO"]] = r_df[
-    MY_COLUMNS["TIPO DE ESTABLECIMIENTO"]
-].str.lower()
-
-r_df[MY_COLUMNS["TELEFONO"]] = r_df[MY_COLUMNS["TELEFONO"]].apply(
-    lambda x: x.replace(".0", "")
+r_df["name"] = r_df["name"].str.lower()
+r_df["direction"] = r_df["direction"].str.lower()
+r_df["type_establishment"] = r_df["type_establishment"].str.lower()
+r_df["type_establishment"] = r_df["type_establishment"].apply(
+    lambda x: x.replace("á", "a")
 )
+r_df = r_df[r_df["type_establishment"].str.contains("restaurante")]
+r_df["phone"] = r_df["phone"].apply(lambda x: x.replace(".0", ""))
+r_df["email"] = r_df["email"].apply(lambda x: x.replace("NO", "N/A"))
 
-r_df[MY_COLUMNS["EMAIL"]] = r_df[MY_COLUMNS["EMAIL"]].apply(
-    lambda x: x.replace("NO", "N/A")
-)
+# restaurants_by_location = (
+#     r_df.groupby(["location", "type_establishment"]).size().unstack()
+# )
+# restaurants_by_location = restaurants_by_location.replace(np.nan, 0)
 
+# fig, ax = plt.subplots(figsize=(12, 6), gridspec_kw={"right": 0.70, "bottom": 0.23})
+# restaurants_by_location.plot(kind="bar", ax=ax)
 
-r_df.pop("DIAS DE ATENCION")
-r_df.pop("SERVICIOS ADICIONALES")
+# ax.set_title("Tipos de Restaurantes por Ubicación")
+# ax.set_xlabel("Ubicación")
+# ax.set_ylabel("Número de Establecimientos")
+# ax.legend(title="Tipo de Establecimiento", bbox_to_anchor=(1.05, 1), loc="upper left")
 
-print(r_df.groupby("location").describe())
+# plt.show()
+
+# grouped_data = r_df.groupby("location")["average_client_day"].mean()
+
+# # Crea un diagrama de sectores con colores diferentes para cada sección
+# plt.figure(figsize=(8, 8))
+# plt.pie(grouped_data, labels=grouped_data.index, autopct="%1.1f%%")
+
+# # Ajusta el diseño y muestra el gráfico
+# plt.title("Diagrama de Sectores por Número Promedio de Clientes y Ubicación")
+# plt.show()
